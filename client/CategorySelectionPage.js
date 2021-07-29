1bd79ade9f0ca3b3472fc581fb3d3d931109a722
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-07-30 04:05:51 +0900

    feat: 각 페이지 라우팅
    필요한 페이지를 구현한 이후 확인할 수 있도록  라우팅합니다.

---
+++ client/CategorySelectionPage.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+import { Text } from 'react-native';
+
+export default function CategorySelectionPage () {
+    return (
+        <Text>질문 카테고리를 설정합니다.</Text>
+    );
+}
