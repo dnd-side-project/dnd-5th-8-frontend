fcdc6e67d9cf4e1bee36070897892c34f12d01a1
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-08-01 00:01:40 +0900

    feat: 패밀리톡 라운드 증감 설정
    UI를 꾸미기전으로 라운드를 증감시킬 수 있도록 기능을 완료하였습니다.

--- client/TalkPage.js
+++ client/TalkPage.js
@@ -1,17 +1,54 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 
-import { Text, Button } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
 
-export default function TalkPage ({ navigation, route }) {
+export default function TalkPage ({ navigation }) {
+  const [state, setState] = useState({
+    round: 1,  
+  });
+
+  const { round } = state;
+
+  const reduceRound = () => {
+    if(round === 1){
+      console.log('더 이상 줄일 수 없습니다.');
+      return;
+    }
+    setState({ round: round - 1})
+  }
+
+  const increaseRound = () => setState({ round: round + 1})
+
     return (
         <>
-        <Text>패밀리톡의 라운드 횟수를 정합니다</Text>
-        <Button
-          title="가족 대기방 이동"
-          onPress={() =>
-            navigation.navigate('Wait')
-          }
-        />
+        <View style={styles.container}>
+          <Text>탐사 횟수를 정해주세요.</Text>
+          <Button
+            title = '-'
+            onPress = {reduceRound}
+          />
+          <Text>{round} 라운드</Text>
+          <Button
+            title = '+'
+            onPress = {increaseRound}
+          />
+          </View>
+
+          <Button
+            title="가족 대기방 이동"
+            onPress={() =>
+              navigation.navigate('Wait')
+            }
+          />
         </>
     );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
