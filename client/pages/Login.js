c94a62ea0d974a58cd7455475fe06315b3a759c9
Solda <solda@khu.ac.kr>
2021-08-01 23:08:43 +0900

    feat:Complete login page with google login using environment variables

--- client/pages/Login.js
+++ client/pages/Login.js
@@ -1,17 +1,27 @@
-import React, { useState } from "react";
-import { Text } from "react-native";
+import React, { useState, useContext } from "react";
+import { Text, ActivityIndicator } from "react-native";
 import { WebView } from "react-native-webview";
 
 import Container from "../components/Container";
 import Contents from "../components/Contents";
 import Button from "../components/Button";
-import SocialWebviewModal from "../pages/SocialWebviewModal";
 
 import styled from "styled-components";
 
 import * as Linking from "expo-linking";
 
+import * as Google from "expo-google-app-auth";
 import axios from "axios";
+
+// credentials context
+import { CredentialsContext } from "../components/CredentialsContext";
+
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import { CLIENT_ID_IOS, CLIENT_ID_ANDROID } from "@env";
+
+// const CLIENT_ID_IOS = "1093078017797-4h18gb8q04qkntiv9sv76afev9810bvd.apps.googleusercontent.com";
+//const CLIENT_ID_ANDROID = "1093078017797-pkdo3e7ukjf8bg7pop01a0cufu8e87ih.apps.googleusercontent.com";
 
 const Title = styled.Text`
   position: absolute;
@@ -61,19 +71,71 @@ const Label = styled.Text`
 `;
 
 export default function Login({ navigation }) {
+  const [hidePassword, setHidePassword] = useState(true);
+  const [message, setMessage] = useState();
+  const [messageType, setMessageType] = useState();
+  const [googleSubmitting, setGoogleSubmitting] = useState(false);
+
+  const { storedCredentials, setStoredCredentials } = useContext(CredentialsContext);
+
+  const handleMessage = (message, type = "") => {
+    setMessage(message);
+    setMessageType(type);
+  };
+
+  const handleGoogleSignin = () => {
+    setGoogleSubmitting(true);
+    const config = {
+      iosClientId: CLIENT_ID_IOS,
+      android: CLIENT_ID_ANDROID,
+      scopes: ["profile", "email"],
+    };
+
+    Google.logInAsync(config)
+      .then((result) => {
+        const { type, user } = result;
+        if (type == "success") {
+          const { email, name, photoUrl } = user;
+          persistLogin({ email, name, photoUrl }, "Google signin successful", "SUCCESS");
+        } else {
+          handleMessage("Google Signin was cancelled");
+        }
+        setGoogleSubmitting(false);
+        alert("로그인 성공!");
+      })
+      .catch((error) => {
+        handleMessage("An error occurred. Check your network and try again");
+        console.log(error);
+        setGoogleSubmitting(false);
+      });
+  };
+
+  const persistLogin = (credentials, message, status) => {
+    AsyncStorage.setItem("flowerCribCredentials", JSON.stringify(credentials))
+      .then(() => {
+        handleMessage(message, status);
+        setStoredCredentials(credentials);
+      })
+      .catch((error) => {
+        handleMessage("Persisting login failed");
+        console.log(error);
+      });
+  };
+
   return (
     <Container>
       <Title>에잇</Title>
-      <KakaoLoginButton>
-        <Label>카카오톡으로 로그인</Label>
-      </KakaoLoginButton>
-      <GoogleLoginButton
-        onPress={() => {
-          navigation.navigate("Profile");
-        }}
-      >
-        <Label>구글로 로그인</Label>
-      </GoogleLoginButton>
+      {!googleSubmitting && (
+        <GoogleLoginButton onPress={handleGoogleSignin} google={true}>
+          <Label google={true}>구글 계정으로 로그인</Label>
+        </GoogleLoginButton>
+      )}
+      {googleSubmitting && (
+        <GoogleLoginButton disabled={true} google={true}>
+          <ActivityIndicator size="large" color={"yellow"} />
+        </GoogleLoginButton>
+      )}
+
       <SignUp>
         <Label style={{ color: "#ff9c78", textDecorationLine: "underline" }}>이메일로 회원가입</Label>
       </SignUp>
