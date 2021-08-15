38efee382a3dacabd4c51bc90e123ab1bdd45d97
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-08-15 16:28:05 +0900

    가족이 맞는지 서버와 통신하여 확인하고 메인홈으로 이동합니다.

--- client/CheckFamilyPage.js
+++ client/CheckFamilyPage.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import axios from 'axios';
+
 import { Text, Button, View, StyleSheet } from 'react-native';
 
 export default function CheckFamilyPage(){
@@ -7,9 +9,23 @@ export default function CheckFamilyPage(){
     const fullName = '홍길동';
     const attendingNum = 1;
 
-    function goFamilyHome() {
-        //Todo: 메인 홈 화면으로 이동
-    };
+    async function goMainHome() {
+        const invitationCode = 'P2SYRO3FNU';
+        const userId = 2;
+        ///daily-questions/update
+        const url = `http://ec2-13-209-36-69.ap-northeast-2.compute.amazonaws.com:8080//space/attend/${userId}`;
+
+        const data = {
+            code : invitationCode,
+        };
+        try {
+          const response = await axios.put(url, data);
+          console.log('요청성공');
+          console.log(response);
+        } catch (error) {
+          console.log(error);
+        }
+      };
 
     function goPreviousScreen() {
         //Todo: 이전 화면으로 이동
@@ -23,7 +39,7 @@ export default function CheckFamilyPage(){
                     <Text>{`${fullName}님외 ${attendingNum}명 참가 중`}</Text>
                     <Button
                         title="우리 가족이 맞아요!"
-                        onPress = {goFamilyHome}
+                        onPress = {goMainHome}
                     />
                     <Button
                         title="이전 화면으로"
