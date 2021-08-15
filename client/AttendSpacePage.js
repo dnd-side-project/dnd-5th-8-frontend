211fc9af40c9647c52550fb79c1160a6d6362ce6
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-08-15 16:18:28 +0900

    스페이스 참가하기 위해 서버로부터 스페이스 정보 얻기

--- client/AttendSpacePage.js
+++ client/AttendSpacePage.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 
+
+import axios from 'axios';
+
 import { 
   Text,
   TextInput,
@@ -8,21 +11,47 @@ import {
 
 export default function AttendSpacePage(){
     const [state, setState] = useState({
-        isShown: false,  
+        isShown: false,
+        inputCode: "",
+        spaceName: "",
+        userCount: 0,
       });
 
-    const { isShown } = state;
+    const { isShown, inputCode } = state;
+
+    const getSpaceInfo = () => {
+        console.log('코드:'+inputCode);
+        console.log(typeof(inputCode));
+        const url = 'http://ec2-13-209-36-69.ap-northeast-2.compute.amazonaws.com:8080/space/attend/' + inputCode; //code는 방생성시 만들어진 코드를 입력해야합니다.
+
+        axios
+            .get(url, {params : { headers: {"Access-Control-Allow-Origin": "*"}}}) //, { headers: {"Access-Control-Allow-Origin": "*"}}
+            .then((res) => {
+                console.log("요청 성공");
+                const data = JSON.stringify(res);
+                const { spaceName, userCount, isExist } = data;
+                console.log(data);
+                setState({
+                    spaceName,
+                    userCount,
+                    isShown: isExist,
+                })
+            })
+            .catch((error) => console.log(error));
+    }
 
     const handleChangeInput= (text) => {
         const isEmpty = text === undefined || text === '';
         if (isEmpty){
             setState({
                 isShown: false,
+                inputCode: "",
             });
             return;
         }
         setState({
             isShown: true,
+            inputCode: text, 
         });
     }
 
@@ -40,6 +69,7 @@ export default function AttendSpacePage(){
             {isShown ? 
                 <Button
                     title="스페이스 참가"
+                    onClick={getSpaceInfo}
                 />
                 :
                 <Button disabled
