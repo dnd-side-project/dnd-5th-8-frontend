1b5d895b888adf32b2c68c391fc4d837a57f1116
HongJungKim <58525009+HongJungKim-dev@users.noreply.github.com>
2021-08-17 15:44:19 +0900

    Revert "Feature/#18 home navigation bottom sheet"

--- client/App.js
+++ client/App.js
@@ -1,26 +1,21 @@
-import * as React from 'react';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import CommunicationList from './CommunicatonList';
-import MySpace from './MySpace';
-import SpaceTalking from './SpaceTalking';
-import DailyQuestion from './DailyQuestion';
-import SpaceLetter from './SpaceLetter';
-import {NavigationContainer} from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const Tab = createBottomTabNavigator();
-
   return (
-    <>
-      <NavigationContainer>
-        <Tab.Navigator initialRouteName="home">
-        <Tab.Screen name="talk" component={SpaceTalking} />
-          <Tab.Screen name="list" component={CommunicationList} />
-          <Tab.Screen name="home" component={DailyQuestion} />
-          <Tab.Screen name="letter" component={SpaceLetter} />
-          <Tab.Screen name="space" component={MySpace} />
-        </Tab.Navigator>
-      </NavigationContainer>
-    </>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
