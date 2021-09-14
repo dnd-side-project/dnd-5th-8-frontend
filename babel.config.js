cdb3494a9ae35444db30585976dcb0035faf7132
JaeWangL <jnso5072@outlook.kr>
2021-09-15 03:07:42 +0900

    [Change] babel plugins

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,14 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        "@babel/plugin-proposal-decorators",
+        {
+          legacy: true,
+        },
+      ],
+      ["@babel/plugin-proposal-optional-catch-binding"],
+    ],
   };
 };
